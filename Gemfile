4e81764bfce22a56c84bdf35171b6a6bbbaca9f5
sukesan1984 <takamikousuke@gmail.com>
2016-05-15 16:22:30 +0900

    add consumer token

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'oauth'
 
+gem 'dotenv'
+
--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'oauth'
+
+YOUR_CONSUMER_KEY = ENV["CONSUMER_KEY"]
+YOUR_CONSUMER_SECRET = ENV["CONSUMER_SECRET"]
+def oauth_consumer
+  return OAuth::Consumer.new(YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET, :site => "https://www.healthplanet.jp/oauth/auth")
+end
 
 get '/' do
   erb :index
 end
+
+get '/healthplanet/auth' do
+  callback_url = "http://localhost:4567/healthplanet/callback"
+  request_token = oauth_consumer.get_request_token(:oauth_callback => callback_url)
+
+  session[:request_token] = request_token
+  session[:request_token_secret] = request_token.secret
+  redirect request_token.authorize_url
+  #"test"
+end
+
